34d38cc31b8673b8958a7cc81483bf9d935ba212
SavioAndres <savioandres@hotmail.com>
2020-06-06 11:40:04 -0300

    Projeto iniciado

---
+++ backend/database/migrations/2020_06_06_043703_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cpf')->length(11)->unique();
+            $table->string('name', 120);
+            $table->string('password');
+            $table->text('api_key');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
